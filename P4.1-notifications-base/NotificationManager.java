01464c1cbb6c75d4fcec3198ba9117e0464d937e
Sergio <sergixsanch@gmail.com>
2026-03-07 14:17:03 +0100

    refactor: adaptar NotificationManager para usar servicios

--- P4.1-notifications-base/NotificationManager.java
+++ P4.1-notifications-base/NotificationManager.java
@@ -1,26 +1,21 @@
-// NotificationManager.java - Código a refactorizar
 public class NotificationManager {
-    // TODO: Separar en clases diferentes: EmailService, SMSService, PushService
-    // TODO: Aplicar patrón Strategy para los tipos de notificación
-    // TODO: Añadir sistema de logs
-    
+
     public void send(String type, String message, String recipient) {
+
+        NotificationService service = null;
+
         if (type.equals("email")) {
-            // Código para enviar email
-            System.out.println("Enviando email a " + recipient + ": " + message);
-            // Lógica compleja de email aquí...
+            service = new EmailService();
         } else if (type.equals("sms")) {
-            // Código para enviar SMS
-            System.out.println("Enviando SMS a " + recipient + ": " + message);
-            // Lógica compleja de SMS aquí...
+            service = new SMSService();
         } else if (type.equals("push")) {
-            // Código para notificación push
-            System.out.println("Enviando push a " + recipient + ": " + message);
-            // Lógica compleja de push aquí...
+            service = new PushService();
+        }
+
+        if (service != null) {
+            service.send(message, recipient);
+        } else {
+            System.out.println("Tipo de notificación no soportado");
         }
     }
-    
-    // TODO: Añadir método para enviar a múltiples destinatarios
-    // TODO: Añadir sistema de reintentos
-    // TODO: Añadir validación de parámetros 
 }
