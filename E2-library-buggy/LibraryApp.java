80c9a2be750c8a48790a8b2fa79b0c6d30e8e971
Sergio <sergixsanch@gmail.com>
2026-03-14 14:12:15 +0100

    fix: resolver conflicto e integrar cambios de main

--- E2-library-buggy/LibraryApp.java
+++ E2-library-buggy/LibraryApp.java
@@ -1,9 +1,10 @@
 // LibraryApp.java - Aplicación principal
 public class LibraryApp {
     public static void main(String[] args) {
+        // Creamos una nueva librería
         Library library = new Library();
         
-        // Agregar algunos libros
+        // Agregar nuestros libros favoritos
         library.addBook(new Book("Clean Code", "Robert Martin", "978-0132350884"));
         library.addBook(new Book("Clean Code", "Robert Martin", "978-0132350884")); // Duplicado
         library.addBook(new Book("Design Patterns", "Gamma et al.", "978-0201633610"));
@@ -12,7 +13,7 @@ public class LibraryApp {
         Book found = library.findBookByTitle("clean code");
         System.out.println("Encontrado: " + (found != null ? found.getTitle() : "null"));
         
-        // Probar préstamo
+        // Comprobamos el préstamo
         Book book = library.findBookByTitle("Clean Code");
         if (book != null) {
             book.borrow();
--- P4.1-notifications-base/Main.java
+++ P4.1-notifications-base/Main.java
@@ -1,9 +1,10 @@
 // Main.java - Programa principal
 public class Main {
     public static void main(String[] args) {
+        // Creamos un nuevo manager
         NotificationManager manager = new NotificationManager();
         
-        // Ejemplos de uso
+        // Probamos todas las funcionalidades
         manager.send("email", "Bienvenido al sistema", "usuario@email.com");
         manager.send("sms", "Tu código es 1234", "+34123456789");
         manager.send("push", "Tienes un nuevo mensaje", "user_device_001");
